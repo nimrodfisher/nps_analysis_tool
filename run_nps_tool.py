1178b1bfdb6a1726ebfd436b31edb30c9d869569
nimrodfisher <140729281+nimrodfisher@users.noreply.github.com>
2024-05-15 20:03:52 +0300

    Update run_nps_tool.py

--- run_nps_tool.py
+++ run_nps_tool.py
@@ -6,6 +6,7 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from collections import Counter
+from nltk import bigrams
 
 # NLTK resources download
 nltk.download('vader_lexicon')
@@ -31,9 +32,8 @@ def calculate_nps(df):
             return 'Passive'
         else:
             return 'Detractor'
-    df['Category'] = df['Score'].apply(nps_category)  # Correcting to 'Category'
+    df['Category'] = df['Score'].apply(nps_category)
     return df
-
 
 def perform_sentiment_analysis(df):
     """Perform sentiment analysis and add sentiment labels to the dataframe."""
@@ -41,7 +41,6 @@ def perform_sentiment_analysis(df):
         lambda score: 'Positive' if score >= 0.05 else 'Negative' if score <= -0.05 else 'Neutral'
     )
     return df
-
 
 def plot_nps_trend(df, interval):
     df['Date'] = pd.to_datetime(df['Date'])
@@ -67,7 +66,6 @@ def plot_nps_trend(df, interval):
                   labels={'index': 'Date', 'value': 'Net Promoter Score'})
     st.plotly_chart(fig, use_container_width=True)
 
-
 def extract_keywords(df, category):
     """Extract keywords from feedback based on the selected category and calculate their frequency."""
     feedback_text = df[df['Category'] == category]['Feedback'].tolist()
@@ -78,27 +76,56 @@ def extract_keywords(df, category):
         words.extend(filtered_words)
     return Counter(words)
 
-def plot_keywords_bubble_chart(keyword_counts, title):
-    """Display keywords frequency as a bubble chart."""
+def plot_keywords_bubble_chart(keyword_counts, title, num_keywords=20):
+    """Display keywords and bi-grams frequency as a bubble chart."""
     if not keyword_counts:
         st.write("No keywords found for this category.")
         return
-    keywords_df = pd.DataFrame(keyword_counts.most_common(20), columns=['Word', 'Frequency'])
-    fig = px.scatter(keywords_df, x='Word', y='Frequency', size='Frequency', color='Word', title=title, size_max=60)
+
+    keywords_df = pd.DataFrame(keyword_counts.most_common(num_keywords), columns=['Phrase', 'Frequency'])
+    fig = px.scatter(keywords_df, x='Phrase', y='Frequency', size='Frequency', color='Phrase', title=title, size_max=num_keywords, hover_data=['Phrase'])
     st.plotly_chart(fig, use_container_width=True)
+
+def extract_keywords_bi_grams(df, category):
+    """Extract bi-gram keywords from feedback based on the selected category and calculate their frequency."""
+    feedback_text = df[df['Category'] == category]['Feedback'].tolist()
+    words = []
+    bi_grams_list = []
+
+    for text in feedback_text:
+        word_tokens = word_tokenize(text.lower())
+        filtered_words = [word for word in word_tokens if word.isalpha() and word not in stop_words]
+
+        # Adding filtered words to the overall list
+        words.extend(filtered_words)
+
+        # Create bi-grams from the filtered list of words
+        bi_grams = list(bigrams(filtered_words))
+        bi_grams_list.extend([' '.join(bi_gram) for bi_gram in bi_grams])
+
+    # Counting single words and bi-grams
+    all_keywords = words + bi_grams_list
+    return Counter(all_keywords)
 
 def main():
     """Main function to run the Streamlit app."""
     st.title('NPS Analyzer Tool')  # App header
-
+    # Adding an emoji as an icon next to the link
+    blog_url = "https://bit.ly/44EqvBb"
+    st.sidebar.markdown(f"📊 [Read my full blog on NPS analysis!]({blog_url})")
     st.sidebar.title('Filter Options')
-    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type="csv")
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file (ID, Date, Product, Feedback, Score)", type="csv")
 
     if uploaded_file is not None:
-        global data
         data = load_data(uploaded_file)
+        # Let users choose the number of top keywords to display
 
-        # data['Date'] = pd.to_datetime(data['Date'], errors='coerce')
+        # Mapping of user-friendly terms to codes
+        interval_options = {
+            'Daily': 'D',
+            'Weekly': 'W',
+            'Monthly': 'M'
+        }
 
         if 'Date' in data.columns and not data['Date'].isna().all():
             # Filter out NaT values if present and get unique dates
@@ -119,31 +146,23 @@ def main():
 
         selected_products = st.sidebar.multiselect('Select Product(s)', options=data['Product'].unique())
 
-        interval = st.sidebar.selectbox('Select aggregation interval', ['D', 'W', 'M'])
-        category_for_keywords = st.sidebar.selectbox('Select a category for keywords analysis', ['Detractor', 'Passive', 'Promoter'])
+        selected_interval_label = st.sidebar.selectbox('Select aggregation interval', list(interval_options.keys()))
+        interval = interval_options[selected_interval_label]
+        # interval = st.sidebar.selectbox('Select aggregation interval', ['D', 'W', 'M'])
+        category_for_keywords = st.sidebar.selectbox('Select a category for keywords analysis', ['Promoter','Passive','Detractor'])
+        num_keywords = st.sidebar.slider("Select number of top phrases", min_value=5, max_value=50, value=20, step=5)
 
         if st.sidebar.button('Analyze Data'):
-            if min_date and max_date:
-                data = data[(data['Date'] >= min_date) & (data['Date'] <= max_date)]
-            if selected_products:
-                data = data[data['Product'].isin(selected_products)]
-
-
             data = calculate_nps(data)
             data = perform_sentiment_analysis(data)
 
             st.subheader('NPS Trend Over Time')
-            st.info("NPS Trend Over Time: This chart shows the net promoter score trend calculated as the percentage of promoters minus the percentage of detractors over time.")
+            st.info("This chart shows the net promoter score trend calculated as the percentage of promoters minus the percentage of detractors over time.")
             plot_nps_trend(data, interval)
 
             st.subheader('Sentiment Analysis Results for All Feedback')
-            st.info("Sentiment Analysis Results: This section visualizes the count and proportion of feedback classified as Positive, Neutral, or Negative based on sentiment analysis.")
             sentiment_counts = data['Sentiment'].value_counts()
             total_feedback = sentiment_counts.sum()
-            sentiment_props = sentiment_counts
-
-
-            # Calculate and display the proportion of each sentiment category
             sentiment_props = (sentiment_counts / total_feedback * 100).round(2)
             sentiment_data = pd.DataFrame({'Sentiment': sentiment_counts.index, 'Counts': sentiment_counts.values,
                                            'Proportion': sentiment_props.values})
@@ -154,25 +173,10 @@ def main():
             st.plotly_chart(fig)
 
             st.subheader('Keyword Analysis for Selected Category')
-            st.info("Keyword Analysis: Displays the most frequent words used in the feedback from a selected product, helping to pinpoint common themes or issues.")
-            # Select category for keyword analysis
-            selected_category = category_for_keywords
-            # data['Category'] = data['Score'].apply(calculate_nps(data))
-            print (selected_category)
-            keyword_counts = extract_keywords(data, selected_category)
-            # Filter data based on selected category
-            # filtered_keywords = data[data['Category'] == selected_category]['Keywords']
-
-            # Extract keywords based on selected category
-            keyword_counts = extract_keywords(data, selected_category)
-
-            # Plot if there are keywords to display
+            st.info("Displays the most frequent words used in the feedback from a selected product, helping to pinpoint common themes or issues.")
+            keyword_counts = extract_keywords(data, category_for_keywords)
             if keyword_counts:
-                plot_keywords_bubble_chart(keyword_counts, f'Keyword Frequency for Category {selected_category}')
-            else:
-                st.write("No keywords found for this category.")
-
-            # Explanation bubbles for each section
+                plot_keywords_bubble_chart(keyword_counts, f'Keyword Frequency for Category {category_for_keywords}', num_keywords)
 
 if __name__ == '__main__':
     main()
